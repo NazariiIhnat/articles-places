81722cff76b63b0303c6c64249094b5e8305efa9
Ihnat1994 <ignatnazar@gmail.com>
2022-05-26 19:32:35 +0200

    Update main frame. Create 2 text fields for searching.

--- gui/src/main/java/components/MainFrame.java
+++ gui/src/main/java/components/MainFrame.java
@@ -1,5 +1,8 @@
 package components;
 
+import components.article.ArticlesTable;
+import components.location.TreeItemList;
+import lombok.AccessLevel;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 import javax.swing.*;
@@ -10,17 +13,21 @@ import javax.swing.border.EmptyBorder;
 public class MainFrame extends JFrame {
 
     private JPanel contentPane;
+    private Menu menu;
     private TreeItemList treeItemList;
+    private JTextField searchLocationTextFiled;
+    private JTextField searchItemTextField;
     private ArticlesTable articlesTable;
-    private JButton generateBtn;
 
     public MainFrame() {
         this.treeItemList = new TreeItemList();
         this.articlesTable = new ArticlesTable();
         initMainFrame();
+        initMenu();
+        initSearchLocationTextField();
+        initSearchItemTextField();
         initTreeItemList();
         initArticlesTable();
-        initGenerateBtn();
     }
 
     public static void main(String[] args) {
@@ -30,26 +37,65 @@ public class MainFrame extends JFrame {
 
     private void initMainFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 409, 339);
+        setBounds(100, 100, 409, 349);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
     }
 
+    private void initMenu() {
+        this.menu = new Menu();
+        menu.setBounds(0, 0, getWidth(), 20);
+        add(menu);
+    }
+
+    private void initSearchLocationTextField() {
+        searchLocationTextFiled = new JTextField();
+        searchLocationTextFiled.setBounds(10, 31, 161, 20);
+        contentPane.add(searchLocationTextFiled);
+    }
+
+    private void initSearchItemTextField() {
+        searchItemTextField = new JTextField();
+        searchItemTextField.setBounds(181, 31, 204, 20);
+        contentPane.add(searchItemTextField);
+    }
+
     private void initTreeItemList() {
-        treeItemList.getScrollPane().setBounds(10, 11, 161, 239);
+        treeItemList.getScrollPane().setBounds(10, 61, 161, 239);
         contentPane.add(treeItemList.getScrollPane());
     }
 
     private void initArticlesTable() {
-        articlesTable.getScrollPane().setBounds(181, 11, 204, 239);
+        articlesTable.getScrollPane().setBounds(181, 61, 204, 239);
         contentPane.add(articlesTable.getScrollPane());
     }
 
-    private void initGenerateBtn() {
-        generateBtn = new JButton("Generuj");
-        generateBtn.setBounds(10, 261, 89, 23);
-        contentPane.add(generateBtn);
+    @Getter
+    public class Menu extends JMenuBar{
+        @Getter(AccessLevel.PACKAGE)
+        private JMenu itemsMenu;
+        private JMenuItem addItemToLocationMenuItem;
+        private JMenuItem removeItemFromLocationMenuItem;
+        private JMenuItem findItemMenuITem;
+        @Getter(AccessLevel.PACKAGE)
+        private JMenu optionsMenu;
+        private JMenuItem openFolderWithBarcodesMenuItem;
+
+        Menu() {
+            this.itemsMenu = new JMenu("Towar");
+            add(itemsMenu);
+            this.addItemToLocationMenuItem = new JMenuItem("Dodaj do lokacji");
+            itemsMenu.add(addItemToLocationMenuItem);
+            this.removeItemFromLocationMenuItem = new JMenuItem("Wyjmij z lokacji");
+            itemsMenu.add(removeItemFromLocationMenuItem);
+            this.findItemMenuITem = new JMenuItem("Znajdż");
+            itemsMenu.add(findItemMenuITem);
+            this.optionsMenu = new JMenu("Opcje");
+            add(optionsMenu);
+            this.openFolderWithBarcodesMenuItem = new JMenuItem("Otwórz folder z kodami lokacji");
+            optionsMenu.add(openFolderWithBarcodesMenuItem);
+        }
     }
 }
